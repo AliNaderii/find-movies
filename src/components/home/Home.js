17a5baf8d6d2e6416cb20f1031000034ef561d86
iAmAli <ali.naderi72@gmail.com>
2022-05-05 02:49:59 +0430

    :lipstick: added an error && title && auto focus on searchbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,25 @@
 // styles
 import './Home.scss';
 // tools
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getAllMovies, getAllSeries, resetState } from '../../features/moviesSlice';
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
+  const [error, setError] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const inputRef = useRef();
 
   const findMovies = (e) => {
     e.preventDefault();
     dispatch(resetState());
 
-    if (searchValue) {
+    if (!searchValue) {
+      setError('You did not searched for anything');
+    } else {
       dispatch(getAllMovies({ term: searchValue }));
       navigate('/list/movies');
     }
@@ -25,14 +29,24 @@ export default function Home() {
     e.preventDefault();
     dispatch(resetState());
 
-    if (searchValue) {
+    if (!searchValue) {
+      setError('You did not searched for anything');
+    } else {
       dispatch(getAllSeries({ term: searchValue }));
-      navigate('/list/series');
+      navigate('/list/movies');
     }
   };
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div className="home-container">
+      <div className='title-container'>
+        <h2 className='title'>What are you looking for?</h2>
+        {error && <p className='error'>{error}</p>}
+      </div>
       <form className='form-container' onSubmit={(e) => e.preventDefault()}>
         <div className='search-container'>
           <input
@@ -41,6 +55,8 @@ export default function Home() {
             placeholder='Search...'
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
+            ref={inputRef}
+            onFocus={() => setError('')}
           />
         </div>
         <div className='btn-container'>
