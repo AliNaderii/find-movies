a1ce95c277b2d6e2a802e63ad753a96daeafe05f
iAmAli <ali.naderi72@gmail.com>
2022-05-04 19:25:59 +0430

    :lipstick: changed style of header/footer && added contact links to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,27 @@
 // styles
 import './Footer.scss';
+// icons
+import { AiOutlineGithub, AiOutlineMail, AiOutlineMobile } from 'react-icons/ai';
 
 export default function Footer() {
   return (
     <div className="footer-container">
-      <p className='footer-logo'>Movie App</p>
       <p className='footer-info'>
         ©2022, Find Me The Show, Inc
       </p>
-    </div>
+      <div className="contact-container">
+        <a
+          href='https://github.com/AliNaderii'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <AiOutlineGithub />
+        </a>
+        <a href='mailto:ali.naderi72@gmail.com'>
+          <AiOutlineMail />
+        </a>
+        <a href='tel:09383086248'><AiOutlineMobile /></a>
+      </div>
+    </div >
   );
 }
